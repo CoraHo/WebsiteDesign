f4c762bfd8c857c548c482f8da3c6e6373e3b2d7
CoraHo <cora9122@gmail.com>
2023-01-03 14:20:08 -0500

    first commit

---
+++ WebsiteDesign_Spring/src/main/java/cora/ho/websitedesign_spring/WebsiteDesignSpringApplication.java
@@ -0,0 +1,13 @@
+package cora.ho.websitedesign_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteDesignSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteDesignSpringApplication.class, args);
+    }
+
+}
---
+++ WebsiteDesign_Spring/src/test/java/cora/ho/websitedesign_spring/WebsiteDesignSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package cora.ho.websitedesign_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteDesignSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
