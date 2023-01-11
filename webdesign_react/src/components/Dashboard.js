3a37ad8541c7967536c57179521c4f2aa0e6e096
CoraHo <cora9122@gmail.com>
2023-01-11 17:56:36 -0500

    built up raw header component

---
+++ webdesign_react/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import HomeLayout from "./HomeLayout";
+
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <HomeLayout/>
+        );
+    }
+}
+
+export default Dashboard;
